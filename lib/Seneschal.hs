26270f3250aedcb7ade632e4ad6f2d226f724616
Adam McCullough <merlinfmct87@gmail.com>
2021-10-01 21:24:15 -0700

    Remove some needless excetpion-happy functions
    `head` -> `headMay` ftw!

--- lib/Seneschal.hs
+++ lib/Seneschal.hs
@@ -1,24 +1,30 @@
 module Seneschal (
-runShellCommandInParallel
+  runShellCommandInParallel,
 ) where
 
-import Control.Concurrent.Async ( mapConcurrently )
-import System.Process ( readProcess )
+import Control.Concurrent.Async (mapConcurrently)
+import Safe (headMay)
+import System.Process (readProcess)
 
 runShellCommandInParallel :: [String] -> IO ()
 runShellCommandInParallel cmds = do
-  outputs <- mapConcurrently (\cmd -> do 
-    let (binName, args) = tokenizeString cmd in readProcess binName args "") cmds
+  outputs <- mapConcurrently parallelCommand cmds
   mapM_ putStrLn outputs
-  
 
--- This function brings Dishonor upon my House. Refactor it to use the Either
--- monad, so my ancestors may look down upon me from Sto'Vo'Kor with pride.
+parallelCommand :: String -> IO String
+parallelCommand cmd =
+  let tokenizeResult = tokenizeString cmd
+      result = case tokenizeResult of
+        Just (binName, args) -> readProcess binName args ""
+        Nothing -> return ""
+   in result
 
-tokenizeString :: String -> (String, [String])
-tokenizeString cmd = 
+tokenizeString :: String -> Maybe (String, [String])
+tokenizeString cmd =
   let cmdList = words cmd
-      binName = head cmdList
-      args = tail cmdList in
-        (binName, args)
-
+      binNameM = headMay cmdList
+      args = tail cmdList
+      result = case binNameM of
+        Just a -> Just (a, args)
+        Nothing -> Nothing
+   in result
