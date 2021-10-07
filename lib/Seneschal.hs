2742953a9677f35eed0e69d74514508f2b99a3d0
Adam McCullough <merlinfmct87@gmail.com>
2021-10-07 00:01:16 -0700

    Refactor the parallel function, handle stdout/stderr better

--- lib/Seneschal.hs
+++ lib/Seneschal.hs
@@ -6,27 +6,42 @@ module Seneschal (
 
 import Control.Monad (forM)
 import Control.Monad.IO.Class (liftIO)
-import Core.Program (None, Program, forkThread, waitThread, writeR)
-import Core.Text (Rope, Textual (fromRope, intoRope), breakLines)
-import System.Process (readProcess)
-import Prelude (Traversable, fmap, mapM_, ($), (<$>), (<>))
+import Core.Program (None, Program, debugS, forkThread, waitThread, writeR)
+import Core.Text (Rope, Textual (fromRope, intoRope))
+import qualified Data.List as List (intercalate)
+import System.Exit (ExitCode (..))
+import System.Process.Typed (closed, proc, readProcess, setStdin)
+import Prelude (Traversable, fmap, mapM_, return, words, ($), (<>))
 
-forkThreadsAndWait :: Traversable f => (f a) -> (a -> Program t b) -> Program t (f b)
+forkThreadsAndWait :: Traversable f => f a -> (a -> Program t b) -> Program t (f b)
 forkThreadsAndWait things action = do
   threads <- forM things $ \thing -> forkThread (action thing)
   forM threads waitThread
 
+{-
+Thin wrapper around **typed-process**'s `readProcess` so that the command
+to be executed can be logged. Bit of an annoyance that the command and the
+arguments have to be specified to `proc` separately, but that's _execvp(3)_
+for you.
+TODO this could potentially move to the **unbeliever** library
+-}
+-- Shamelessly stolen from https://github.com/aesiniath/publish/blob/main/src/Utilities.hs#L41-L61
+execProcess :: Rope -> Program t (ExitCode, Rope, Rope)
+execProcess cmd =
+  let cmdStrs = words $ fromRope cmd
+      task = proc "bash" ("-c" : cmdStrs)
+      task' = setStdin closed task
+      command = List.intercalate " " ("bash" : "-c" : cmdStrs)
+   in do
+        debugS "command" command
+
+        (exit, out, err) <- liftIO $ do
+          readProcess task'
+
+        return (exit, intoRope out, intoRope err)
+
 parallel :: [Rope] -> Program None ()
 parallel cmds = do
-  outputs <- forkThreadsAndWait cmds parallelCommand
-  mapM_ writeR outputs
-
-parallelCommand :: Rope -> Program None Rope
-parallelCommand cmd =
-  let cmdLines = breakLines cmd
-      cmdStrs = fmap fromRope cmdLines
-      -- This is great, and it _almost_ works right, but it still kinda doesn't.
-      -- `stdout` is great, outputs are not trampled, but `stderr` comes out
-      -- live, which isn't great
-   in liftIO $ intoRope <$> readProcess "bash" (["-c"] <> cmdStrs) ""
-
+  outputs <- forkThreadsAndWait cmds execProcess
+  let outputsOuts = fmap (\(_exitCode, out, err) -> out <> "\n" <> err) outputs
+   in mapM_ writeR outputsOuts
